af427e410c2ce7e96cc86421305226eaa8207e8b
Alex Mills <git@viper007bond.com>
2014-04-25 16:52:37 -0700

    Settings API Improvements
    * Internal cache functionality to avoid so many calls to `get_option()` which is cached but has some overhead.
    * New reset_all() method that clears out a user's own settings.
    * New maybe_upgrade() method that upgrades the settings in the database. So far it just migrats `shversion` to `renderer`.

--- classes/class-settings.php
+++ classes/class-settings.php
@@ -1,30 +1,30 @@
 <?php
 
 class SyntaxHighlighter_Settings {
-	public $version      = '4.0.0-alpha';
 	public $setting_name = 'syntaxhighlighter_settings';
+	public $version = '4';
+
+	public $settings = array();
+
+	public function __construct() {
+		$this->maybe_upgrade();
+
+		$this->settings = $this->get_settings_with_defaults();
+	}
 
 	public function __get( $name ) {
-		$settings = $this->get_settings_with_defaults();
-
-		// Deprecated settings
-		switch ( $name ) {
-			case 'shversion':
-				return ( isset( $settings['renderer'] ) && 'sh2' == $settings['renderer'] ) ? 2 : 3;
-		}
-
-		if ( array_key_exists( $name, $settings ) ) {
-			return $settings[ $name ];
+		if ( array_key_exists( $name, $this->settings ) ) {
+			return $this->settings[ $name ];
 		} else {
 			return null;
 		}
 	}
 
 	public function __set( $name, $value ) {
+		$this->settings[ $name ] = $value;
+
 		$settings = $this->get_settings_without_defaults();
-
 		$settings[ $name ] = $value;
-
 		update_option( $this->setting_name, $settings );
 	}
 
@@ -44,24 +44,62 @@ class SyntaxHighlighter_Settings {
 
 	public function get_defaults( $defaults = array() ) {
 		$defaults = wp_parse_args( $defaults, array(
-				'theme'          => 'default',
-				'loadallbrushes' => 0,
-				'renderer'       => 'sh3',
-				'title'          => '',
-				'autolinks'      => 1,
-				'classname'      => '',
-				'collapse'       => 0,
-				'firstline'      => 1,
-				'gutter'         => 1,
-				'htmlscript'     => 0,
-				'light'          => 0,
-				'padlinenumbers' => 'false',
-				'smarttabs'      => 1,
-				'tabsize'        => 4,
-				'toolbar'        => 0,
-				'wraplines'      => 1, // 2.x only
-			) );
+			'theme'          => 'default',
+			'loadallbrushes' => 0,
+			'renderer'       => 'sh3',
+			'title'          => '',
+			'autolinks'      => 1,
+			'classname'      => '',
+			'collapse'       => 0,
+			'firstline'      => 1,
+			'gutter'         => 1,
+			'htmlscript'     => 0,
+			'light'          => 0,
+			'padlinenumbers' => 'false',
+			'smarttabs'      => 1,
+			'tabsize'        => 4,
+			'toolbar'        => 0,
+			'wraplines'      => 1, // SH 2.x only
+		) );
 
 		return (array) apply_filters( 'syntaxhighlighter_defaultsettings', $defaults );
+	}
+
+	public function reset_all() {
+		update_option( $this->setting_name, array() );
+		$this->settings = $this->get_settings_with_defaults();
+	}
+
+	public function maybe_upgrade() {
+		$settings = $this->get_settings_without_defaults();
+
+		if ( empty( $settings['version'] ) ) {
+			$settings['version'] = 0;
+		}
+
+		$modified = false;
+
+		if ( $settings['version'] < 4 ) {
+			if ( ! empty( $settings['shversion'] ) ) {
+				switch ( $settings['shversion'] ) {
+					case 2:
+						$settings['renderer'] = 'sh2';
+						break;
+
+					case 3:
+						$settings['renderer'] = 'sh3';
+						break;
+
+				}
+
+				unset( $settings['shversion'] );
+
+				$modified = true;
+			}
+		}
+
+		if ( $modified ) {
+			update_option( $this->setting_name, $settings );
+		}
 	}
 }
