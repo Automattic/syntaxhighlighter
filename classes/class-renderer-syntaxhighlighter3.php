035bac3c328957bbc9dfaf1ad5fa951478b67722
Alex Mills <git@viper007bond.com>
2014-12-02 15:13:53 -0800

    SH3: Remove some shortcodes we don't want to use and introduce the set_defaults() method.

--- classes/class-renderer-syntaxhighlighter3.php
+++ classes/class-renderer-syntaxhighlighter3.php
@@ -15,12 +15,44 @@ class SyntaxHighlighter_Renderer_SH3 extends SyntaxHighlighter_Renderer {
 	public function set_shortcodes() {
 		parent::set_shortcodes();
 
-		// Remove some shortcodes we don't want while still supporting them as language values
-		//unset( $this->shortcodes[ array_search( 'latex', $this->shortcodes ) ] ); // Remove "latex" shortcode (it'll collide with rendering shortcodes)
-		//unset( $this->shortcodes[ array_search( 'r', $this->shortcodes ) ] );     // Remove "r" shortcode (too short)
+		# Remove some shortcodes we don't want while still supporting them as language values
+
+		// "latex" will often collide with LaTeX rendering shortcodes
+		if ( in_array( 'latex', $this->shortcodes ) ) {
+			unset( $this->shortcodes[ array_search( 'latex', $this->shortcodes ) ] );
+		}
+
+		// "r" is just too short to be useful
+		if ( in_array( 'r', $this->shortcodes ) ) {
+			unset( $this->shortcodes[ array_search( 'r', $this->shortcodes ) ] );
+		}
 	}
 
-	public function shortcode_callback( $atts, $code = '', $tag = false ) {
-		return 'Hello world!';
+	public function set_defaults( $atts ) {
+		$defaults = array(
+			'language'       => null,
+			'lang'           => null,
+			'type'           => null, // deprecated language alias
+
+			'autolinks'      => null,
+			'classname'      => null,
+			'collapse'       => null,
+			'firstline'      => null,
+			'gutter'         => null,
+			'highlight'      => null,
+			'htmlscript'     => null,
+			'light'          => null,
+			'padlinenumbers' => null,
+			'quickcode'      => null,
+			'smarttabs'      => null,
+			'tabsize'        => null,
+			'title'          => null,
+			'toolbar'        => null,
+			'unindent'       => null,
+		);
+
+		$atts = apply_filters( 'syntaxhighlighter_shortcodeatts', shortcode_atts( $defaults, $atts ) );
+
+		return $atts;
 	}
 }
