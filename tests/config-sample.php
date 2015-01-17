4a98d484b708cd297c6b0e4d6c8b8825c3fc7575
Alex Mills <git@viper007bond.com>
2015-01-16 19:08:20 -0800

    Unit Tests: Introduce optional config.php file for easily changing WordPress tests directory.

---
+++ tests/config-sample.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * By default these unit tests will try to load a copy of WordPress from
+ * /tmp/wordpress-tests-lib. If you wish to change that, you can do so here.
+ */
+putenv( 'WP_TESTS_DIR=/foo/bar' );
