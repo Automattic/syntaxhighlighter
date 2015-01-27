88f25e6fd096aafc18f6c57e005bd9a85cc6577f
Alex Mills <git@viper007bond.com>
2015-01-26 21:29:32 -0800

    Bump version numbers

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -4,7 +4,7 @@
 
 Plugin Name:  SyntaxHighlighter Evolved
 Plugin URI:   http://www.viper007bond.com/wordpress-plugins/syntaxhighlighter/
-Version:      3.1.10
+Version:      3.1.11
 Description:  Easily post syntax-highlighted code to your site without having to modify the code at all. Uses Alex Gorbatchev's <a href="http://alexgorbatchev.com/wiki/SyntaxHighlighter">SyntaxHighlighter</a>. <strong>TIP:</strong> Don't use the Visual editor if you don't want your code mangled. TinyMCE will "clean up" your HTML.
 Author:       Alex Mills (Viper007Bond)
 Author URI:   http://www.viper007bond.com/
@@ -21,7 +21,7 @@ Thanks to:
 
 class SyntaxHighlighter {
 	// All of these variables are private. Filters are provided for things that can be modified.
-	var $pluginver            = '3.1.10';  // Plugin version
+	var $pluginver            = '3.1.11';  // Plugin version
 	var $agshver              = false;    // Alex Gorbatchev's SyntaxHighlighter version (dynamically set below due to v2 vs v3)
 	var $shfolder             = false;    // Controls what subfolder to load SyntaxHighlighter from (v2 or v3)
 	var $settings             = array();  // Contains the user's settings
@@ -111,7 +111,7 @@ class SyntaxHighlighter {
 			$this->agshver  = '2.1.364';
 		} else {
 			$this->shfolder = 'syntaxhighlighter3';
-			$this->agshver  = '3.0.9';
+			$this->agshver  = '3.0.9b';
 		}
 
 		// Register brush scripts
