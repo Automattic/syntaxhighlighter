3db624f928791fa87267b74a6ce05ebf02c9b8c3
Alex Mills <git@viper007bond.com>
2015-01-30 23:47:16 -0800

    Screw old versions of PHP. I'm going to use __DIR__.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( file_exists( dirname( __FILE__ ) . '/config.php' ) ) {
-	include_once( dirname( __FILE__ ) . '/config.php' );
+if ( file_exists( __DIR__ . '/config.php' ) ) {
+	include_once( __DIR__ . '/config.php' );
 }
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
@@ -13,7 +13,7 @@ require_once( $_tests_dir . '/includes/functions.php' );
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/syntaxhighlighter.php';
+	require_once( dirname( __DIR__ ) . '/syntaxhighlighter.php' );
 }
 
 require_once( $_tests_dir . '/includes/bootstrap.php' );
