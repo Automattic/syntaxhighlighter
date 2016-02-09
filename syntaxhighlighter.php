2dd839ee85a861573cf8ea27520f12b5b9257443
Alex Mills <git@viper007bond.com>
2016-02-09 12:15:19 -0800

    Sync do_shortcode_keep_escaped_tags() method with core's do_shortcode()

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -344,15 +344,34 @@ class SyntaxHighlighter {
 
 	// This is a clone of do_shortcode() that uses a different callback function
 	// The new callback function will keep escaped tags escaped, i.e. [[foo]]
-	// Up to date as of r18324 (3.2)
-	function do_shortcode_keep_escaped_tags( $content ) {
+	// Up to date as of r36097
+	function do_shortcode_keep_escaped_tags( $content,  $ignore_html = false  ) {
 		global $shortcode_tags;
+
+		if ( false === strpos( $content, '[' ) ) {
+			return $content;
+		}
 
 		if (empty($shortcode_tags) || !is_array($shortcode_tags))
 			return $content;
 
-		$pattern = get_shortcode_regex();
-		return preg_replace_callback('/'.$pattern.'/s', array( $this, 'do_shortcode_tag_keep_escaped_tags' ), $content);
+		// Find all registered tag names in $content.
+		preg_match_all( '@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches );
+		$tagnames = array_intersect( array_keys( $shortcode_tags ), $matches[1] );
+
+		if ( empty( $tagnames ) ) {
+			return $content;
+		}
+
+		$content = do_shortcodes_in_html_tags( $content, $ignore_html, $tagnames );
+
+		$pattern = get_shortcode_regex( $tagnames );
+		$content = preg_replace_callback( "/$pattern/", array( $this, 'do_shortcode_tag_keep_escaped_tags' ), $content );
+
+		// Always restore square braces so we don't break things like <!--[if IE ]>
+		$content = unescape_invalid_shortcodes( $content );
+
+		return $content;
 	}
 
 
