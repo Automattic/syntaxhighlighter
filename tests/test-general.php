e5b33ad5930ff2718335a4edb5ae0501ae9b07c0
Alex Mills <Viper007Bond@users.noreply.github.com>
2018-12-08 15:32:56 -0800

    Add a Travis CI build to verify no parse errors.

---
+++ tests/test-general.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class SyntaxHighlighter_Evolved_Tests_General
+ *
+ * @package    SyntaxHighlighter_Evolved
+ */
+
+require_once dirname( __FILE__ ) . '/helper.php';
+
+/**
+ * Tests for SyntaxHighlighter Evolved.
+ */
+class SyntaxHighlighter_Evolved_Tests_General extends WP_UnitTestCase {
+	public function test_verify_no_parse_errors() {
+		$this->assertTrue( true );
+	}
+}
