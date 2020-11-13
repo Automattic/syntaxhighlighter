a9f11b3b0c156d3697e16bd364a03c3443ac376a
Peter Kiss <peter@yscik.com>
2020-11-13 14:53:35 +0100

    Move block to dedicated folder

---
+++ src/code-block/index.js
@@ -0,0 +1,63 @@
+import { __ } from '@wordpress/i18n';
+import deprecated from './deprecated';
+import edit from './edit';
+import save from './save';
+import transforms from './transforms';
+
+const { settings } = window.syntaxHighlighterData;
+
+export default {
+	title: __( 'SyntaxHighlighter Code', 'syntaxhighlighter' ),
+	description: __( 'Adds syntax highlighting to source code (front end only).', 'syntaxhighlighter' ),
+	icon: 'editor-code',
+	category: 'formatting',
+	keywords: [
+		// translators: Keyword that user might search for when trying to locate block.
+		__( 'Source', 'syntaxhighlighter' ),
+		// translators: Keyword that user might search for when trying to locate block.
+		__( 'Program', 'syntaxhighlighter' ),
+		// translators: Keyword that user might search for when trying to locate block.
+		__( 'Develop', 'syntaxhighlighter' ),
+	],
+	attributes: {
+		content: {
+			type: 'string',
+			source: 'text',
+			selector: 'code',
+		},
+		language: {
+			type: 'string',
+			default: settings.language.default,
+		},
+		lineNumbers: {
+			type: 'boolean',
+			default: settings.lineNumbers.default,
+		},
+		firstLineNumber: {
+			type: 'string',
+			default: settings.firstLineNumber.default,
+		},
+		highlightLines: {
+			type: 'string',
+		},
+		wrapLines: {
+			type: 'boolean',
+			default: settings.wrapLines.default,
+		},
+		makeURLsClickable: {
+			type: 'boolean',
+			default: settings.makeURLsClickable.default,
+		},
+		quickCode: {
+			type: 'boolean',
+			default: settings.quickCode.default,
+		},
+	},
+	supports: {
+		html: false,
+	},
+	transforms,
+	edit,
+	save,
+	deprecated,
+};
