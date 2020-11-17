1827ce29c5fb5b14e06265ed0c824232cdf09536
Peter Kiss <peter@yscik.com>
2020-11-17 14:56:06 +0100

    Remove link protocol slashes escaping

--- src/utils.js
+++ src/utils.js
@@ -17,26 +17,5 @@ import { escapeEditableHTML } from '@wordpress/escape-html';
 export function escape( content ) {
 	return flow(
 		escapeEditableHTML,
-		escapeProtocolInIsolatedUrls
 	)( content || '' );
-}
-/**
- * Converts the first two forward slashes of any isolated URL into their HTML
- * counterparts (i.e. // => &#47;&#47;). For instance, https://youtube.com/watch?x
- * becomes https:&#47;&#47;youtube.com/watch?x.
- *
- * An isolated URL is a URL that sits in its own line, surrounded only by spacing
- * characters.
- *
- * See https://github.com/WordPress/wordpress-develop/blob/5.1.1/src/wp-includes/class-wp-embed.php#L403
- *
- * @param {string}  content The content of a code block.
- * @return {string} The given content with its ampersands converted into
- *                  their HTML entity counterpart (i.e. & => &amp;)
- */
-function escapeProtocolInIsolatedUrls( content ) {
-	return content.replace(
-		/^(\s*https?:)\/\/([^\s<>"]+\s*)$/m,
-		'$1&#47;&#47;$2'
-	);
 }
