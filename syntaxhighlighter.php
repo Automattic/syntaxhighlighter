fbd9962c050f2111b494cdaeea88fbc0acbb2c3e
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-12-03 20:29:06 -0800

    Ensure HTML special characters are not already encoded before they are encoded and output by the render callback
    I believe this will address the issues described in #108 and #159. At first I thought I would just add `$code = str_replace( '&gt;', '>', $code );` as a third line of "Undo escaping...", but then I figured it would potentially catch more edge cases to just decode all HTML entities at this point in the code. They get re-encoded later in the `shortcode_callback` method, so this helps further prevent double-encoding.

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -530,8 +530,7 @@ class SyntaxHighlighter {
 		$code = preg_replace( '#<pre [^>]+>([^<]+)?</pre>#', '$1', $content );
 
 		// Undo escaping done by WordPress
-		$code = str_replace( '&lt;', '<', $code );
-		$code = str_replace( '&amp;', '&', $code );
+		$code = htmlspecialchars_decode( $code );
 
 		return $this->shortcode_callback( $attributes, $code, 'code' );
 	}
