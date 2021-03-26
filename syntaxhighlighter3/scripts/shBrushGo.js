a0b3058e1a184361f337b74295e3b6f3ae06e4df
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-03-26 17:02:06 +0200

    Replace with single quotes

--- syntaxhighlighter3/scripts/shBrushGo.js
+++ syntaxhighlighter3/scripts/shBrushGo.js
@@ -16,36 +16,32 @@
  */
 (function () {
 	// CommonJS
-	SyntaxHighlighter =
-		SyntaxHighlighter ||
-		(typeof require !== "undefined"
-			? require("shCore").SyntaxHighlighter
-			: null);
+	SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
 
 	function Brush() {
 		var keywords =
-			"bool break byte case chan complex128 complex64 const continue default defer else " +
-			"fallthrough float32 float64 for func go goto if import int int16 int32 int64 int8 " +
-			"interface map package range return rune select string struct switch type uint " +
-			"uint16 uint32 uint64 uint8 uintptr var";
+			'bool break byte case chan complex128 complex64 const continue default defer else ' +
+			'fallthrough float32 float64 for func go goto if import int int16 int32 int64 int8 ' +
+			'interface map package range return rune select string struct switch type uint ' +
+			'uint16 uint32 uint64 uint8 uintptr var';
 		var funcs =
-			"append cap close complex copy imag len make new panic print println real recover delete";
-		var special = "true false iota nil";
+			'append cap close complex copy imag len make new panic print println real recover delete';
+		var special = 'true false iota nil';
 
 		this.regexList = [
 			{
 				regex: SyntaxHighlighter.regexLib.singleLineCComments,
-				css: "comments",
+				css: 'comments',
 			}, // one line comments
-			{ regex: /\/\*([^\*][\s\S]*?)?\*\//gm, css: "comments" }, // multiline comments
-			{ regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm, css: "preprocessor" }, // documentation comments
-			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: "string" }, // strings
-			{ regex: SyntaxHighlighter.regexLib.singleQuotedString, css: "string" }, // strings
-			{ regex: XRegExp("`([^\\\\`]|\\\\.)*`", "gs"), css: "string" }, // strings
-			{ regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi, css: "value" }, // numbers
-			{ regex: new RegExp(this.getKeywords(keywords), "gm"), css: "keyword" }, // keywords
-			{ regex: new RegExp(this.getKeywords(funcs), "gmi"), css: "functions" }, // built-in functions
-			{ regex: new RegExp(this.getKeywords(special), "gm"), css: "color1" }, // literals
+			{ regex: /\/\*([^\*][\s\S]*?)?\*\//gm, css: 'comments' }, // multiline comments
+			{ regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm, css: 'preprocessor' }, // documentation comments
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' }, // strings
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' }, // strings
+			{ regex: XRegExp('`([^\\\\`]|\\\\.)*`', 'gs'), css: 'string' }, // strings
+			{ regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi, css: 'value' }, // numbers
+			{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }, // keywords
+			{ regex: new RegExp(this.getKeywords(funcs), 'gmi'), css: 'functions' }, // built-in functions
+			{ regex: new RegExp(this.getKeywords(special), 'gm'), css: 'color1' }, // literals
 		];
 
 		this.forHtmlScript({
@@ -55,10 +51,10 @@
 	}
 
 	Brush.prototype = new SyntaxHighlighter.Highlighter();
-	Brush.aliases = ["go", "golang"];
+	Brush.aliases = ['go', 'golang'];
 
 	SyntaxHighlighter.brushes.Go = Brush;
 
 	// CommonJS
-	typeof exports != "undefined" ? (exports.Brush = Brush) : null;
+	typeof exports != 'undefined' ? (exports.Brush = Brush) : null;
 })();
