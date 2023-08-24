520c80b1c75e9453a7eb93dd70ac7a9f458e62a8
Fernando Jorge Mota <contato@fjorgemota.com>
2023-08-24 19:45:15 -0300

    Improve regex to handle code snippets "<" when using the syntax highlighter block

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -554,7 +554,7 @@ class SyntaxHighlighter {
 			}
 		}
 
-		$code = preg_replace( '#<pre [^>]+>([^<]+)?</pre>#', '$1', $content );
+		$code = preg_replace( '#<pre [^>]+>(.*)</pre>#s', '$1', $content );
 
 		// Undo escaping done by WordPress
 		$code = htmlspecialchars_decode( $code );
