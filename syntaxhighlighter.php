a34760407ffad6e4053af29b4732c81db397e5c8
Fernando Jorge Mota <fernando.jorge.mota@automattic.com>
2023-12-04 18:49:01 -0300

    Update "Requires at least" field to 6.1

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -11,7 +11,7 @@ Author URI:   https://alex.blog/
 Text Domain:  syntaxhighlighter
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Requires at least: 5.7
+Requires at least: 6.1
 Tested up to: 6.4
 Requires PHP: 7.0
 
